d8482ae137df2ca59ca50088cd6d5c36ea230bd3
sobhan9012 <sobhansahoo2000@gmail.com>
2024-06-30 22:05:20 +0530

    adding all the pictures

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 export default function App() {
   return (
     <>
-      <div className="text-blue-400">
+      <div className="text-blue-400 text-3xl m-9 p-10">
         sobhan sahoo
       </div>
     </>
